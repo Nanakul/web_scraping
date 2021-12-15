03e1bc75e95329470d51450975de94c94d18d352
Midi Nanakul <midinanakul@hotmail.com>
2021-12-15 00:02:42 -0800

    Working on grabbing images.

--- web_scraper.py
+++ web_scraper.py
@@ -14,3 +14,10 @@ web_class = soup.select('.toc')[0]
 # for loop to grab all text within the toc class
 for item in web_class:
     print(item.text)
+
+# Grabbing an image (using new website than above)
+second_url = 'https://en.wikipedia.org/wiki/Diablo_IV'
+second_response = requests.get(second_url)
+second_soup = bs4.BeautifulSoup(second_response.text, 'lxml')
+second_web_first_image = second_soup.select('.thumbinner')[0]
+print(second_web_first_image)
