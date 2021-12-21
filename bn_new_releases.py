8de1f48c3559b1892ba329dc4173a9e17a0a5e1d
Midi Nanakul <midinanakul@hotmail.com>
2021-12-20 19:00:22 -0800

    Added function and variables to begin scraping BN.

--- bn_new_releases.py
+++ bn_new_releases.py
@@ -1,3 +1,10 @@
 import bs4
 import requests
 
+
+def new_release():
+    """This function will grab the book name and respective price."""
+    bn_url = 'https://www.barnesandnoble.com/b/books/_/N-1fZ1sZ29Z8q8?Nrpp=20&page={}'
+    bn_req = requests.get(bn_url.format(1))
+    bn_soup = bs4.BeautifulSoup(bn_req.text, 'lxml')
+    
