5d78cff88bdc6e676abe3b77106950e05c878836
Midi Nanakul <midinanakul@hotmail.com>
2021-12-20 22:12:47 -0800

    Reformatted HEADERS and changed the book_title variable.

--- bn_new_releases.py
+++ bn_new_releases.py
@@ -4,16 +4,18 @@ import requests
 
 def new_release():
     """This function will grab the book name and respective price."""
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)AppleWebKit/537.36 (KHTML, '
-                             'like Gecko)Chrome/96.0.4664.110 Safari/537.36',
-               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,'
-                         '*/*;q=0.8,application/'}
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
+                             'AppleWebKit/537.36 (KHTML, like Gecko)'
+                             'Chrome/96.0.4664.110 Safari/537.36',
+               'accept': 'text/html,application/xhtml+xml,'
+                         'application/xml;q=0.9,image/avif,'
+                         'image/webp,image/apng,*/*;q=0.8,application/'}
     bn_url = 'https://www.barnesandnoble.com/b/books/_/N-1fZ1sZ29Z8q8?Nrpp=20&page={}'
     bn_resp = requests.get(bn_url.format(1), headers=HEADERS)
     bn_soup = bs4.BeautifulSoup(bn_resp.text, 'lxml')
 
     # Grab the title.
-    book_title = bn_soup.select('.product-shelf-title product-info-title pt-xs')
+    book_title = bn_soup.select('.product-shelf-title')
     print(len(book_title))
 
 
